009f5a0b1908acd6ea0a01b4376ca99b643925b8
imduchuyyy <duchuy.124dk@gmail.com>
2024-11-18 18:15:31 +0700

    feat(cli): add commands

--- apps/cli/main.ts
+++ apps/cli/main.ts
@@ -24,6 +24,15 @@ class HelixCLI {
     this.#registerCommand("help", this.handleHelp.bind(this), "Print all available commands");
     this.#registerCommand("create", this.handleCreateWallet.bind(this), "Create a new wallet");
     this.#registerCommand("address", this.handleGetDefaultAddress.bind(this), "Get default address");
+    this.#registerCommand("addresses", this.handleGetAllAddresses.bind(this), "Get all addresses created");
+    this.#registerCommand("set-address", this.handleSetDefaultAddress.bind(this), "Set default address");
+    this.#registerCommand("add-address", this.handleAddAddress.bind(this), "Add address");
+    this.#registerCommand("send", this.handleSend.bind(this), "Send transaction");
+    this.#registerCommand("balance", this.handleFetchBalance.bind(this), "Fetch balance");
+    this.#registerCommand("port", this.handleFetchPortfolio.bind(this), "Fetch portfolio");
+    this.#registerCommand("networks", this.handleGetNetworks.bind(this), "Get all networks");
+    this.#registerCommand("network", this.handleGetDefaultNetwork.bind(this), "Get default networks");
+    this.#registerCommand("set-network", this.handleSetDefaultNetwork.bind(this), "Set default network");
   }
 
   #registerCommand(name: string, handler: CommandHandler, description: string) {
@@ -122,6 +131,46 @@ class HelixCLI {
 
   async handleGetDefaultAddress(args: string[]) {
     console.log("Get address");
+  }
+
+  async handleSetDefaultAddress(args: string[]) {
+    console.log("set address");
+  }
+
+  async handleAddAddress(args: string[]) {
+    console.log("add address");
+  }
+
+  async handleGetAllAddresses(args: string[]) {
+    console.log("Get addresses");
+  }
+
+  async handleFetchBalance(args: string[]) {
+    console.log("Fetch balance");
+  }
+
+  async handleFetchPortfolio(args: string[]) {
+    console.log("Fetch portfolio");
+  }
+
+  async handleGetNetworks(args: string[]) {
+    console.log("Get networks");
+  }
+
+  async handleGetDefaultNetwork(args: string[]) {
+    console.log("Get default networks");
+  }
+
+  async handleChangeNetwork(args: string[]) {
+    console.log("change networks");
+  }
+
+  async handleSetDefaultNetwork(args: string[]) {
+    console.log("set network");
+  }
+
+  async handleSend(args: string[]) {
+    console.log("Send");
   }
 }
 
